eb5a3f2c96eca9b02fce1ae8aa69bcb1865925ad
Aleix Riba <aleix.riba@addcode.es>
2021-01-24 01:03:11 +0100

    structure has been created for registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'register', pathMatch: 'full'},
+  {path: 'register', loadChildren: () => import('./modules/register/register.module').then((m) => m.RegisterModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
